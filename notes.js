554a2464957e5d6e0633320c50953d2ba9df363c
DamonFoster <dfoster27@utexas.edu>
2018-07-24 19:35:42 -0700

    added list function and removed extra console.logs

--- notes.js
+++ notes.js
@@ -1,5 +1,3 @@
-console.log('Starting notes.js');
-
 const fs = require('fs');
 
 var fetchNotes = () => {
@@ -32,7 +30,7 @@ var addNote = (title, body) => {
 };
 
 var getAll = () => {
-	console.log('Getting all notes');
+	return fetchNotes();
 };
 
 var getNote = (title) => {
@@ -52,6 +50,7 @@ var removeNote = (title) => {
 };
 
 var logNote = (note) => {
+	debugger;
 	console.log('_ _ _');
 	console.log(`Title: ${note.title}`);
 	console.log(`Body: ${note.body}`);
